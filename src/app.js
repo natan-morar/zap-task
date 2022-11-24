ade0de46d52eb5f24c8123c19d7f9646a14b4566
Diego Zacarias <511910+zv3@users.noreply.github.com>
2022-11-24 03:19:27 -0300

    Adding the `/zaptic` endpoint to return the logo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import path from 'path';
 const port = 3333;
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello from Zaptic! 👋💜");
+});
+
+app.get("/zaptic", (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'src/public/res/zaptic_logo.jpeg'));
 });
 
 app.listen(port, () => {
