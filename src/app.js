f70dff6eed75ad8b352eabba6cf8fde68d2f9b74
Alex <17277800+alex-schaaf@users.noreply.github.com>
2023-01-26 16:16:17 +0100

    feat: add /zaptic endpoint serving logo jpg

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+import fs from 'fs'
+import path from 'path'
 import express from "express";
+import { fileURLToPath } from 'url';
+
 const port = 3333;
 const app = express();
 
+const CURRENT_DIR = import.meta.url.substring(0, import.meta.url.lastIndexOf("/"))
+const STATIC_DIR = path.join(fileURLToPath(CURRENT_DIR), "public/res")
+
 app.get("/", (req, res) => {
     res.send("Hello from Zaptic! 👋💜");
+});
+
+app.get("/zaptic", (req, res) => {
+    const filename = "zaptic_logo.jpeg"
+    const stream = fs.createReadStream(path.join(STATIC_DIR, filename))
+    stream.on('open', () => {
+        res.set('Content-Type', 'image/jpg');
+        stream.pipe(res);
+        res.status(200)
+    });
 });
 
 app.listen(port, () => {
